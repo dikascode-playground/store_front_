6c09ef313ea46a4bd5b8e0649345952c598402a8
emmanuel_ututu <dikaemanuel@gmail.com>
2026-06-19 01:22:11 +0100

    feat: add theming, localization, and UI polish

---
+++ app/src/main/java/com/ibi/storefront/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.ibi.storefront.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue80 = Color(0xFFB6D5FF)
+val BlueGrey80 = Color(0xFFC4D3E6)
+val Sky80 = Color(0xFFBFE4FF)
+
+val Blue40 = Color(0xFF0D5BD7)
+val BlueGrey40 = Color(0xFF47617E)
+val Sky40 = Color(0xFF1676F3)
+
+val BlueLightBackground = Color(0xFFF4F8FF)
+val BlueLightSurface = Color(0xFFFFFFFF)
+val BlueLightSurfaceVariant = Color(0xFFE8F1FF)
+val BlueLightOutline = Color(0xFFB8C8E0)
+
+val BlueDarkBackground = Color(0xFF0C1524)
+val BlueDarkSurface = Color(0xFF132033)
+val BlueDarkSurfaceVariant = Color(0xFF1C2D45)
+val BlueDarkOutline = Color(0xFF4B6384)
+
+val FavoriteRed = Color(0xFFE0475B)
